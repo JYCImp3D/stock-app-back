186e871333ad713e75c463a54cb7747523e724dc
JuanBG20 <juanbgalvan.19@gmail.com>
2023-10-16 17:16:32 -0300

    Movement

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,5 @@
-const { trusted } = require("mongoose");
 const Product = require("../models/product");
+const Moveme4 = require("../models/movement");
 
 const getProducts = async (req, res) => {
   const products = await Product.find({ deleted: false }).sort({ _id: -1 });
@@ -36,4 +36,33 @@ const deleteProduct = async (req, res) => {
   res.status(200).json({ ok: true, message: "Producto eliminado con exito" });
 };
 
-module.exports = { getProducts, createProducts, deleteProduct };
+const createMovement = (req, res) => {
+  const { productId } = req.params;
+
+  const newMovement = new Movement({ ...req.body, product: productId });
+
+  newMovement
+    .save()
+    .then((movement) => {
+      res.status(201).json({ ok: true, movement });
+    })
+    .catch((err) => console.log(err));
+};
+
+const deleteMovement = async (req, res) => {
+  const { id } = req.params;
+
+  await Movement.findByIdAndUpdate(id, {
+    deleted: true,
+  });
+
+  res.status(200).json({ ok: true, message: "Movimiento eliminado con exito" });
+};
+
+module.exports = {
+  getProducts,
+  createProducts,
+  deleteProduct,
+  createMovement,
+  deleteMovement,
+};
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -5,4 +5,7 @@ router.post("/", productController.createProducts);
 router.get("/", productController.getProducts);
 router.delete("/:id", productController.deleteProduct);
 
+router.post("/movement/:productId", productController.createMovement);
+router.delete("/movement/:id", productController.deleteMovement);
+
 module.exports = router;
